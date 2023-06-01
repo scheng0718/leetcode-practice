9b957479711c13dd9fa27ce1150df1b335cc5eee
evancheng73 <evancheng73@gmail.com>
2023-06-01 11:17:15 -0700

    create solution for leetcode 35

---
+++ src/LC_35.java
@@ -0,0 +1,26 @@
+public class LC_35 {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        if (target <= nums[left]) {
+            return left;
+        } else if (target <= nums[right]) {
+            return right;
+        } else {
+            return right + 1;
+        }
+    }
+}
