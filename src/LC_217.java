10351eb994b715ee7d11753f3b9d9be61b32a0a2
evancheng73 <evancheng73@gmail.com>
2023-06-01 10:15:10 -0700

    create solution for leetcode 217

---
+++ src/LC_217.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LC_217 {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return true;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+    // 一行寫法，利用Arrays.stream()的方法，但是runtime/memory表現不好
+    // Java 會在比較前進行型別轉換。但如果 distinct().count() 的結果超過了 int 的範圍，則可能會導致失真或溢位的問題
+//    public boolean containsDuplicate(int[] nums) {
+//        if (nums == null || nums.length == 0) {
+//            return true;
+//        }
+//        return nums.length != Arrays.stream(nums).distinct().count();
+//    }
+}
