da5c116a1a0fa8ddb06e50c8b9d45619d3db17ff
evancheng73 <evancheng73@gmail.com>
2023-10-19 15:34:26 -0700

    implement leetcode 154

---
+++ src/LC_154.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] > nums[right]) {
+                left = middle + 1;
+            } else if (nums[middle] < nums[right]) {
+                right = middle;
+            } else {
+                right--;
+            }
+        }
+        return nums[left];    
+    }
+}
---
+++ src/LC_154.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    if (!nums || nums.length === 0) return -1;
+    let left = 0;
+    let right = nums.length - 1;
+    while (left < right) {
+        const middle = Math.floor((left + right) / 2);
+        if (nums[middle] > nums[right]) {
+            left = middle + 1;
+        } else if (nums[middle] < nums[right]) {
+            right = middle;
+        } else {
+            right--;
+        }
+    }    
+    return nums[left];
+};
